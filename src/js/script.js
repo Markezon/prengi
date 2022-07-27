895f129e4b3e90d66fd2de733c9e35b81f76d91b
Mark <markezon2@gmail.com>
2022-07-28 00:14:49 +0200

    add swiper

--- src/js/script.js
+++ src/js/script.js
@@ -59,3 +59,27 @@ menu.addEventListener('click', (e) => {
 		unlockScroll();
 	}
 });
+
+//slider
+
+const slider = document.querySelector('.swiper-container');
+
+let swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+/*   breakpoints: {
+    992: {
+      slidesPerView: 1,
+      centeredSlides: true,
+      spaceBetween: 50,
+    }
+  }, */
+});
